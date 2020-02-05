9802771cb3c6beb6244c09c72edaa96f52edc6e3
Bastien Pic <bpic@pcalb-mm0000.univ-jfc.fr>
2020-02-05 11:42:42 +0100

    ajout best title

--- tuto_git/Application.java
+++ tuto_git/Application.java
@@ -18,7 +18,7 @@ public class Application extends Frame implements WindowListener{
 		Vue vue = new Vue();
 		modl.addObserver(vue);
 		vue.addActionListener(ctrl);
-		this.setTitle("tutoriel git");
+		this.setTitle("Mr. Panzoli buton");
 		this.add(vue);
 		this.pack();
 		this.setVisible(true);
