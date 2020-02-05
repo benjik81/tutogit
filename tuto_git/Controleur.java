e507957fc72ef2808632c5002a30e3921e7dc090
Benjamin Zanini <bzanini@pcalb-mm0000.univ-jfc.fr>
2020-02-05 11:02:25 +0100

    first

---
+++ tuto_git/Controleur.java
@@ -0,0 +1,20 @@
+package tuto_git;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Controleur implements ActionListener {
+
+	Modele refModl;
+	
+	public Controleur(Modele m) {
+		this.refModl = m;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		refModl.incrémente();
+	}
+
+	
+}
